a95583e00c5301a558478b29adcf7547e4fab42e
minxd95 <newobj95@gmail.com>
2023-03-26 15:29:18 +0900

    lottiefiles 추가

--- src/pages/tags.tsx
+++ src/pages/tags.tsx
@@ -1,14 +1,25 @@
 import Layout from "@/components/Layout";
 import SEO from "@/components/SEO";
+import { Player } from "@lottiefiles/react-lottie-player";
+import { Link } from "gatsby";
 import "twin.macro";
 
 const TagsPage = () => {
   return (
     <Layout>
-      <div tw="w-full h-[calc(100vh - 120px)] flex justify-center items-center text-4xl font-bold text-center leading-[72px]">
-        🙏
-        <br />
-        준비중입니다.
+      <div tw="w-full h-[calc(100vh - 120px)] flex flex-col justify-center items-center">
+        <Player
+          autoplay
+          loop
+          src="https://assets5.lottiefiles.com/packages/lf20_iYvSqSMKZB.json"
+          tw="w-[300px] h-[300px]"
+        />
+        <span tw="text-2xl font-medium text-center">
+          sry, I'm developing this page :(
+        </span>
+        <Link tw="mt-6" to="/">
+          go to main 👈
+        </Link>
       </div>
     </Layout>
   );
